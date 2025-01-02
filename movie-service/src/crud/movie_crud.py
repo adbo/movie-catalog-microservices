cc78e75f64cdc73321efb23eecc869ae76efe43e
Adam Borowski <adborows@gmail.com>
2025-01-03 00:00:42 +0100

    feat: searching by actor name

--- movie-service/src/crud/movie_crud.py
+++ movie-service/src/crud/movie_crud.py
@@ -1,12 +1,14 @@
 from sqlmodel import Session, select
 from typing import List
-from sqlalchemy import or_
+from sqlalchemy import func
 
 from ..models.movie import Movie
 
 def search_movies(db: Session, query: str) -> List[Movie]:
     statement = select(Movie).where(
-        (Movie.title.ilike(f"%{query}%")) | (Movie.description.ilike(f"%{query}%"))
+        (Movie.title.ilike(f"%{query}%")) | 
+        (Movie.description.ilike(f"%{query}%")) |
+        (func.array_to_string(Movie.actors, ', ').ilike(f"%{query}%"))
     )
     results = db.exec(statement)
     return results.all()
